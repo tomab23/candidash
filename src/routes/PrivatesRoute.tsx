e86b26e783f1d58f0ea8fd5605a85c45ded14515
thomas bartier <thomas.bartier@gmail.com>
2025-09-21 21:37:43 +0200

    trad loading privatesRoutes

--- src/routes/PrivatesRoute.tsx
+++ src/routes/PrivatesRoute.tsx
@@ -1,12 +1,14 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "@/context/AuthContext"
 import type { JSX } from "react"
+import { useTranslation } from "react-i18next"
 
 export default function PrivateRoute({ children }: { children: JSX.Element }) {
   const { user, loading } = useAuth()
+  const { t } = useTranslation()
 
   if (loading) {
-    return <div>Chargement...</div>
+    return <div className="text-center">{t("LOADING")}...</div>
   }
 
   if (!user) {
