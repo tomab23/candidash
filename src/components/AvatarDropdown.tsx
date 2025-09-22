d9337945ecb5e843709ba2696141c3ec4d50173e
thomas bartier <thomas.bartier@gmail.com>
2025-09-22 11:39:18 +0200

    translate avatar dropdown

--- src/components/AvatarDropdown.tsx
+++ src/components/AvatarDropdown.tsx
@@ -6,15 +6,26 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Archive, Home, List, LogOut, Settings, User, UserRound } from "lucide-react";
+import {
+  Archive,
+  Home,
+  List,
+  LogOut,
+  Mail,
+  Settings,
+  User,
+  UserRound,
+} from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import { useAuth } from "@/context/AuthContext";
+import { useTranslation } from "react-i18next";
 
 const AvatarDropdown = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const handleLogout = () => {
     logout();
@@ -36,23 +47,26 @@ const AvatarDropdown = () => {
         <DropdownMenuLabel>{user?.email}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={() => navigate("/home")}>
-          <Home className="h-4 w-4" /> Home
+          <Home className="h-4 w-4" /> {t("TITLE.HOME")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => navigate("/profile")}>
-          <User className="h-4 w-4" /> Profile
+          <User className="h-4 w-4" /> {t("TITLE.PROFILE")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => navigate("/settings")}>
-          <Settings className="h-4 w-4" /> Settings
+          <Settings className="h-4 w-4" /> {t("TITLE.SETTINGS")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => navigate("/archives")}>
-          <Archive className="h-4 w-4" /> Archive
+          <Archive className="h-4 w-4" /> Archives
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => navigate("/updates")}>
-          <List className="h-4 w-4"/> Updates
+          <List className="h-4 w-4" /> {t("TITLE.UPDATES")}
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => navigate("/")} disabled>
+          <Mail className="h-4 w-4" /> Contact
         </DropdownMenuItem>
         <Separator />
         <DropdownMenuItem className="text-destructive" onClick={handleLogout}>
-          <LogOut className="h-4 w-4 stroke-destructive" /> Logout
+          <LogOut className="h-4 w-4 stroke-destructive" /> {t("TITLE.LOGOUT")}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
