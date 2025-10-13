6e024cb7de85f0941636707497dcd95a29e65f73
thomas bartier <thomas.bartier@gmail.com>
2025-10-13 14:23:54 +0200

    test ui

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-              server: {
-        host: '192.168.1.72'
-      },
+      //         server: {
+      //   host: ''
+      // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
