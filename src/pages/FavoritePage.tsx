1e478d1984d5e04adb7638b2ba3555c418f98ab4
thomas bartier <thomas.bartier@gmail.com>
2026-07-08 07:44:44 +0200

    see favorites page favorites with filter

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,16 +1,83 @@
+import CandidatureCard from "@/components/cards/CandidatureCard";
 import Header from "@/components/layout/Header";
-import Navbar from "@/components/layout/Navbar"
-import Contenu from "@/helpers/Contenu"
-import { useTranslation } from "react-i18next"
+import Navbar from "@/components/layout/Navbar";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Contenu from "@/helpers/Contenu";
+import { useCandidature } from "@/hooks/useCandidature";
+import { useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+type Filter = "application" | "interest";
 
 export const FavoritesPage = () => {
   const { t } = useTranslation();
+  const { candidatures, interests } = useCandidature();
+  const [filter, setFilter] = useState<Filter>(
+    "application",
+  );
+
+  const favoriteApplication = useMemo(
+    () => candidatures.filter((c) => c.favorite),
+    [candidatures],
+  );
+
+  const favoriteInterest = useMemo(
+    () => interests.filter((c) => c.favorite),
+    [interests],
+  );
+
+  const data = filter === "application" ? favoriteApplication : favoriteInterest;
+
+  const items = [
+    { label: t("CANDIDATURE"), value: "application" },
+    { label: t("INTEREST.TITLE"), value: "interest" },
+  ];
+
   return (
     <div>
       <Navbar />
       <Contenu>
         <Header title={t("TITLE.FAVORITES")} />
+
+        <div className="flex justify-end mt-2">
+          <Select
+            value={filter}
+            onValueChange={(value) =>
+              setFilter(value as "application" | "interest")
+            }
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectGroup>
+                {items.map((item) => (
+                  <SelectItem key={item.value} value={item.value}>
+                    <p className="capitalize">{item.label}</p>
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
+            {data.map((c) => (
+              <CandidatureCard key={c.id} candidature={c} />
+            ))}
+          </div>
+
+          {data.length < 1 && <p>{t("LIST.FAVORITE")}</p>}
+  
       </Contenu>
     </div>
-  )
-}
+  );
+};
--- src/pages/NotesPage.tsx
+++ src/pages/NotesPage.tsx
@@ -1,16 +1,25 @@
 import NoteCard from "@/components/cards/NoteCard";
 import Header from "@/components/layout/Header";
 import Navbar from "@/components/layout/Navbar";
+import { Button } from "@/components/ui/button";
 import Contenu from "@/helpers/Contenu";
+import { useTranslation } from "react-i18next";
 
 const NotesPage = () => {
+  const { t } = useTranslation();
   return (
     <div>
       <Navbar />
       <Contenu>
         <Header title={"Notes"} />
 
-        <div>
+        <div className="flex justify-between items-center mt-2">
+          <p className="text-xl">Vous avez 0 note</p>
+
+          <Button>{t("BUTTON.NOTE")}</Button>
+        </div>
+
+        <div className="mt-10">
           <NoteCard />
         </div>
       </Contenu>
